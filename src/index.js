f0cf92c2f9a2a43415c47d7b67612550533c6ccb
Diogenes Monegro <diogenes_mj@hotmail.com>
2022-03-17 20:37:37 -0400

    fix: change url

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ const TOKEN = process.env.TOKEN;
 const CHAT_ID = process.env.CHAT_ID;
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-const url = "https://www.codewars.com/collections"
+const url = "https://www.codewars.com/kata/latest"
 
-const randomItem = arr => arr[Math.floor(Math.random() * arr.length)];
+// const randomItem = arr => arr[Math.floor(Math.random() * arr.length)];
 
 cron.schedule("*/10 * * * * *", () => {
     axios.get(url).then(({ data }) => {
         const $ = cheerio.load(data);
-        const challenger = randomItem($(".item-title a"))       
-        bot.sendMessage(CHAT_ID, `Buen dia y Bendiciones\nReto del dia: https://www.codewars.com/${challenger.attr("href")}`);
+        const challenger = $(".item-title a")[0].attribs.href;       
+        bot.sendMessage(CHAT_ID, `Buen dia y Bendiciones\nReto del dia: https://www.codewars.com${challenger}`);
     }).catch(err => console.log(err));
 });
